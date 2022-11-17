5a95e9b04581440e54317f2ac810154578435322
Noa Midzic <nmidzic@yahoo.com>
2022-11-17 11:40:48 +0100

    Camera preview
    Will show camera preview when camera button is pressed from Home Activity

--- Software/GeoGallery/app/src/main/java/org/foi/rampu/geogallery/CameraActivity.kt
+++ Software/GeoGallery/app/src/main/java/org/foi/rampu/geogallery/CameraActivity.kt
@@ -81,7 +81,33 @@ class CameraActivity : AppCompatActivity() {
         }
     }
 
-    private fun startCamera() {}
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview)
+
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+    }
+
     private fun takePhoto() {}
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
