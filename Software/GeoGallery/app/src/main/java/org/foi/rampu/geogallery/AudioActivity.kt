5c3544dc34f80dfce0ee8d2d29ef31c00812614e
Hrvoje Lukšić <hluksic20@foi.hr>
2022-11-18 19:54:00 +0100

    Added empty audio recording activity #4

---
+++ Software/GeoGallery/app/src/main/java/org/foi/rampu/geogallery/AudioActivity.kt
@@ -0,0 +1,11 @@
+package org.foi.rampu.geogallery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AudioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_audio)
+    }
+}
