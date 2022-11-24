7676d3e38ebbd377b0e18e97eae04c1d854daeac
David Kajzogaj <dkajzogaj20@foi.hr>
2022-11-24 20:31:34 +0100

    Removed firebase, and implemented new google login #3

--- Software/GeoGallery/app/src/main/java/org/foi/rampu/geogallery/MainActivity.kt
+++ Software/GeoGallery/app/src/main/java/org/foi/rampu/geogallery/MainActivity.kt
@@ -4,16 +4,13 @@ import android.content.ContentValues
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.StrictMode
 import android.util.Log
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.GoogleAuthProvider
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import org.foi.rampu.geogallery.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -21,28 +18,28 @@ class MainActivity : AppCompatActivity() {
     private lateinit var viewBinding: ActivityMainBinding
 
     private lateinit var googleSignInClient: GoogleSignInClient
-    private lateinit var auth: FirebaseAuth
-
-    companion object{
-        const val RC_SIGN_IN = 1001
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
-
-        auth = Firebase.auth
+        StrictMode.enableDefaults()
 
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken("826124397115-k6sgb8csrs0q86tfg6racm6t37ove33p.apps.googleusercontent.com")
             .requestEmail()
             .build()
 
         googleSignInClient = GoogleSignIn.getClient(this, gso)
 
+        val acct : GoogleSignInAccount? = GoogleSignIn.getLastSignedInAccount(this)
+
+        if(acct != null){
+            updateUI()
+        }
+
         viewBinding.btnSignIn.setOnClickListener{
-            signIn()
+            val signInIntent = googleSignInClient.signInIntent
+            startActivityForResult(signInIntent, 1001)
         }
 
         viewBinding.btnTest.setOnClickListener {
@@ -51,52 +48,24 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun signIn() {
-        val signInIntent = googleSignInClient.signInIntent
-        startActivityForResult(signInIntent, RC_SIGN_IN)
-    }
-
-    override fun onStart() {
-        super.onStart()
-        updateUI(auth.currentUser)
-    }
-
-    private fun updateUI(user: FirebaseUser?) {
-        if(user != null){
-            startActivity(Intent(this, HomeActivity::class.java))
-            finish()
-        }
-    }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if(requestCode == RC_SIGN_IN){
+        if(requestCode == 1001){
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
             try{
                 val account = task.getResult(ApiException::class.java)!!
-                Log.d(ContentValues.TAG, "firebaseAuthWithGoogle" + account.id)
-                firebaseAuthWithGoogle(account.idToken!!)
+                Log.d(ContentValues.TAG, "Google sign in successful" + account.id)
+                updateUI()
             }catch (e: ApiException){
                 Log.w(ContentValues.TAG, "Google sign in failed", e)
             }
         }
     }
 
-    private fun firebaseAuthWithGoogle(idToken: String) {
-        val credential = GoogleAuthProvider.getCredential(idToken, null)
-        auth.signInWithCredential(credential)
-            .addOnCompleteListener(this){ task ->
-                if(task.isSuccessful){
-                    Log.d(ContentValues.TAG, "signInWithCredential: success")
-                    val user = auth.currentUser
-                    updateUI(user)
-                }else{
-                    Log.w(ContentValues.TAG, "signInWithCredential: failure", task.exception)
-                    updateUI(null)
-                }
-            }
+    private fun updateUI() {
+        startActivity(Intent(this, HomeActivity::class.java))
+        finish()
     }
-
 }
 
