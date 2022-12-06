89d0c8056001ca5bcd11f7eef538f60eedd58902
Noa Midzic <nmidzic@yahoo.com>
2022-12-07 00:31:11 +0100

    Added thumbnails in place of video display #10

--- Software/GeoGallery/app/src/main/java/org/foi/rampu/geogallery/GalleryActivity.kt
+++ Software/GeoGallery/app/src/main/java/org/foi/rampu/geogallery/GalleryActivity.kt
@@ -1,18 +1,24 @@
 package org.foi.rampu.geogallery
 
-import android.R
 import android.content.ContentUris
+import android.graphics.Bitmap
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
+import android.util.Size
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.GridLayout
 import android.widget.ImageView
 import android.widget.MediaController
 import android.widget.Toast
 import android.widget.VideoView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isInvisible
 import org.foi.rampu.geogallery.databinding.ActivityGalleryBinding
 
 
@@ -20,6 +26,7 @@ class GalleryActivity : AppCompatActivity() {
 
     private lateinit var viewBinding: ActivityGalleryBinding
 
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityGalleryBinding.inflate(layoutInflater)
@@ -59,6 +66,7 @@ class GalleryActivity : AppCompatActivity() {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.Q)
     fun display_videos()
     {
         val projection = arrayOf(MediaStore.Video.Media._ID)
@@ -83,13 +91,18 @@ class GalleryActivity : AppCompatActivity() {
 
                 val videoUri = Uri.parse(contentUri.toString())
                 Log.i("URI", videoUri.toString())
-                createVideoView(videoUri)
+
+                val thumbnail = applicationContext.contentResolver.loadThumbnail(videoUri, Size(500, 500), null)
+
+                createVideoView(videoUri, thumbnail)
+
 
             }
         }
+
     }
 
-    fun createVideoView(videoUri : Uri)
+    fun createVideoView(videoUri : Uri, thumbnail : Bitmap)
     {
         val layout = findViewById<View>(org.foi.rampu.geogallery.R.id.gridLayout) as ViewGroup
         val videoView = VideoView(this)
@@ -103,9 +116,13 @@ class GalleryActivity : AppCompatActivity() {
         videoView.setVideoURI(videoUri)
         videoView.layoutParams.height = 500
         videoView.layoutParams.width = 500
-        //videoView.scal = ImageView.ScaleType.FIT_XY
+        videoView.isInvisible = true
+        //videoView.scal = ImageView.ScaleType.FIT_XY FIX VIDEO SIZE
 
-        layout.addView(videoView)
+        //surround image view & video view with framelayout, image view is for thumbnail
+        //when user clicks play, image view -> invisible, video view -> visible & opposite for stop
+        createThumbnail(thumbnail, videoView, layout)
+
         Log.i("videoview", videoView.toString())
 
         setVideoMargins(videoView)
@@ -114,7 +131,52 @@ class GalleryActivity : AppCompatActivity() {
         videoView.setMediaController(mediaController)
 
         //videoView.start();
+        //imageView.setImageBitmap(ThumbnailUtils.createVideoThumbnail(urlPath, MediaStore.Video.Thumbnails.MICRO_KIND))
+        //bitmap = ThumbnailUtils.createVideoThumbnail(getRealPathFromURI(videoUri), MediaStore.Images.Thumbnails.MINI_KIND);
 
+    }
+
+    fun createThumbnail(thumbnail: Bitmap, videoView: VideoView, layout: ViewGroup)
+    {
+        val frameLayout = FrameLayout(this)
+        frameLayout.layoutParams =
+            ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+            )
+
+        val ivThumbnail = ImageView(this)
+        ivThumbnail.layoutParams =
+            ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+            )
+        ivThumbnail.layoutParams.height = 500
+        ivThumbnail.layoutParams.width = 500
+        ivThumbnail.scaleType = ImageView.ScaleType.FIT_XY
+        ivThumbnail.setImageBitmap(thumbnail)
+        ivThumbnail.setColorFilter(R.color.blue)
+
+        /*val playIcon = ImageView(this)
+        playIcon.layoutParams =
+            ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+            )
+        playIcon.layoutParams.height = 500
+        playIcon.layoutParams.width = 500
+        playIcon.scaleType = ImageView.ScaleType.FIT_XY
+        playIcon.setImageBitmap(thumbnail)
+        playIcon.setColorFilter(R.color.blue)*/
+
+        frameLayout.addView(ivThumbnail)
+        frameLayout.addView(videoView)
+        layout.addView(frameLayout)
+
+        //margins
+        /*val param = frameLayout.layoutParams as ViewGroup.MarginLayoutParams
+        param.setMargins(20,20,20,20)
+        frameLayout.layoutParams = param*/
     }
 
 
