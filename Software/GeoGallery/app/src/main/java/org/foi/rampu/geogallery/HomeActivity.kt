c33bb57d5ff55e0aa45ce333e3e9821576270837
Noa Midzic <nmidzic@yahoo.com>
2022-12-06 17:56:35 +0100

    Added displaying photo from MediaStorage on HomeActivity imageView #10

--- Software/GeoGallery/app/src/main/java/org/foi/rampu/geogallery/HomeActivity.kt
+++ Software/GeoGallery/app/src/main/java/org/foi/rampu/geogallery/HomeActivity.kt
@@ -1,8 +1,12 @@
 package org.foi.rampu.geogallery
 
+import android.content.ContentUris
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.ktx.auth
@@ -34,6 +38,42 @@ class HomeActivity : AppCompatActivity() {
 
             Firebase.auth.signOut()
             startActivity(Intent(applicationContext, MainActivity::class.java))
+
+        }
+
+        display_photos()
+
+    }
+
+    fun display_photos()
+    {
+        val projection = arrayOf(MediaStore.Images.Media._ID)
+        val selection : String? = null
+        val selectionArgs = arrayOf<String>()
+        val sortOrder : String? = null
+
+        applicationContext.contentResolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            projection,
+            selection,
+            selectionArgs,
+            sortOrder
+        )?.use { cursor ->
+            while (cursor.moveToNext()) {
+                val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
+                val id = cursor.getLong(idColumn)
+                var contentUri: Uri = ContentUris.withAppendedId(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    id
+                )
+
+
+                val imgUri = Uri.parse(contentUri.toString())
+                Log.i("URI", imgUri.toString())
+                viewBinding.imageView.setImageURI(null)
+                viewBinding.imageView.setImageURI(imgUri)
+
+            }
         }
     }
 }
