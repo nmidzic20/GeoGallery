0e87fd25cbd903ebaa96fe6c99057a9c8ef7dd3b
Noa Midzic <nmidzic@yahoo.com>
2022-12-06 23:42:30 +0100

    Implemented video display in gallery, media controls #10

--- Software/GeoGallery/app/src/main/java/org/foi/rampu/geogallery/GalleryActivity.kt
+++ Software/GeoGallery/app/src/main/java/org/foi/rampu/geogallery/GalleryActivity.kt
@@ -1,18 +1,20 @@
 package org.foi.rampu.geogallery
 
+import android.R
 import android.content.ContentUris
-import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.MediaController
 import android.widget.Toast
-import androidx.core.view.marginBottom
+import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
 import org.foi.rampu.geogallery.databinding.ActivityGalleryBinding
+
 
 class GalleryActivity : AppCompatActivity() {
 
@@ -24,6 +26,7 @@ class GalleryActivity : AppCompatActivity() {
         setContentView(viewBinding.root)
 
         display_photos()
+        display_videos()
     }
 
     fun display_photos()
@@ -54,6 +57,72 @@ class GalleryActivity : AppCompatActivity() {
 
             }
         }
+    }
+
+    fun display_videos()
+    {
+        val projection = arrayOf(MediaStore.Video.Media._ID)
+        val selection : String? = null
+        val selectionArgs = arrayOf<String>()
+        val sortOrder : String? = null
+
+        applicationContext.contentResolver.query(
+            MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+            projection,
+            selection,
+            selectionArgs,
+            sortOrder
+        )?.use { cursor ->
+            while (cursor.moveToNext()) {
+                val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Video.Media._ID)
+                val id = cursor.getLong(idColumn)
+                var contentUri: Uri = ContentUris.withAppendedId(
+                    MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                    id
+                )
+
+                val videoUri = Uri.parse(contentUri.toString())
+                Log.i("URI", videoUri.toString())
+                createVideoView(videoUri)
+
+            }
+        }
+    }
+
+    fun createVideoView(videoUri : Uri)
+    {
+        val layout = findViewById<View>(org.foi.rampu.geogallery.R.id.gridLayout) as ViewGroup
+        val videoView = VideoView(this)
+        videoView.layoutParams =
+            ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+            )
+
+        videoView.setVideoURI(null)
+        videoView.setVideoURI(videoUri)
+        videoView.layoutParams.height = 500
+        videoView.layoutParams.width = 500
+        //videoView.scal = ImageView.ScaleType.FIT_XY
+
+        layout.addView(videoView)
+        Log.i("videoview", videoView.toString())
+
+        setVideoMargins(videoView)
+
+        val mediaController = MediaController(this)
+        videoView.setMediaController(mediaController)
+
+        //videoView.start();
+
+    }
+
+
+    fun setVideoMargins(videoView : VideoView)
+    {
+        val param = videoView.layoutParams as ViewGroup.MarginLayoutParams
+        param.setMargins(20,20,20,20)
+        videoView.layoutParams = param
     }
 
     fun createImageView(imgUri : Uri)
