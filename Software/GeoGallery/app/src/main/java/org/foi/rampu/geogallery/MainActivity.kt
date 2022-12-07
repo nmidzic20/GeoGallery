b7f96c099c21596e94290a814fda43882577e497
David Kajzogaj <dkajzogaj20@foi.hr>
2022-12-08 00:17:36 +0100

    Login and register with Firebase #3

--- Software/GeoGallery/app/src/main/java/org/foi/rampu/geogallery/MainActivity.kt
+++ Software/GeoGallery/app/src/main/java/org/foi/rampu/geogallery/MainActivity.kt
@@ -1,17 +1,9 @@
 package org.foi.rampu.geogallery
 
-import android.content.ContentValues
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.StrictMode
-import android.util.Log
 import android.widget.Toast
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -21,8 +13,6 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var viewBinding: ActivityMainBinding
 
-    private lateinit var googleSignInClient: GoogleSignInClient
-    val Req_Code: Int = 123
     private lateinit var firebaseAuth : FirebaseAuth
 
 
@@ -38,27 +28,14 @@ class MainActivity : AppCompatActivity() {
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
 
-
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id))
-            .requestEmail()
-            .build()
-
-        googleSignInClient = GoogleSignIn.getClient(this, gso)
-
         firebaseAuth = Firebase.auth
-        //firebaseAuth = FirebaseAuth.getInstance()
 
-        viewBinding.btnSignIn.setOnClickListener{
-            signInGoogle()
-        }
-
-        viewBinding.btnTest.setOnClickListener {
-            startActivity(Intent(this, HomeActivity::class.java))
+        viewBinding.tvNoAccount.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
             finish()
         }
 
-        viewBinding.btnZadnji.setOnClickListener {
+       viewBinding.btnLogin.setOnClickListener {
             performLogin()
         }
     }
@@ -78,33 +55,15 @@ class MainActivity : AppCompatActivity() {
         firebaseAuth.signInWithEmailAndPassword(emailInput,passwordInput)
             .addOnCompleteListener(this){ task ->
                 if(task.isSuccessful){
-                    val user = firebaseAuth.currentUser
                     updateUI()
                 }
                 else{
                     Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
                 }
             }
-    }
-
-    private fun signInGoogle() {
-        val signInIntent : Intent = googleSignInClient.signInIntent
-        startActivityForResult(signInIntent,Req_Code)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if(requestCode == 123){
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            try{
-                val account = task.getResult(ApiException::class.java)!!
-                Log.d(ContentValues.TAG, "Google sign in successful" + account.id)
-                updateUI()
-            }catch (e: ApiException){
-                Log.w(ContentValues.TAG, "Google sign in failed", e)
+            .addOnFailureListener{
+                Toast.makeText(baseContext, "Authentication failed: ${it.localizedMessage}", Toast.LENGTH_SHORT).show()
             }
-        }
     }
 
     private fun updateUI() {
