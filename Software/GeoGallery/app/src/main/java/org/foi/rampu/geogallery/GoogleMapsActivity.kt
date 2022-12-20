fb0de87414a7efc846e5e339c181ec1ba35cbe1e
David Kajzogaj <dkajzogaj20@foi.hr>
2022-12-20 22:58:06 +0100

    Added references for google maps in build gradle, api key in Android manifest and created new GoogleMaps activity for google maps #8

---
+++ Software/GeoGallery/app/src/main/java/org/foi/rampu/geogallery/GoogleMapsActivity.kt
@@ -0,0 +1,11 @@
+package org.foi.rampu.geogallery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GoogleMapsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_google_maps)
+    }
+}
